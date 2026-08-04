d5e5cd78bcbebdf10a595cbd409d0c960fb5484e
avof <avof@ukr.net>
2026-08-04 21:00:46 +0200

    setup-web script ready

--- terraform/CryptoPulse/test/vpc/application/ec2_web_asg/setup-web.sh
+++ terraform/CryptoPulse/test/vpc/application/ec2_web_asg/setup-web.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y apache2
+set -euxo pipefail
 
-systemctl enable --now apache2
+APP_ALB_DNS="api.cryptopulse.internal"
+GIT_REPO_URL="https://github.com/avof23/CryptoPulse-DevOps-Lab.git"
+TARGET_DIR="frontend"
+WEB_ROOT="/var/www/html"
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install -y apache2 git
+
+a2enmod proxy
+a2enmod proxy_http
+
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot ${WEB_ROOT}
+
+    <Directory ${WEB_ROOT}>
+        Options -Indexes +FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ProxyPreserveHost On
+    ProxyRequests Off
+
+    ProxyPass /api/ http://${APP_ALB_DNS}:8000/api/
+    ProxyPassReverse /api/ http://${APP_ALB_DNS}:8000/api/
+
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+rm -rf ${WEB_ROOT}/*
+
+mkdir -p /tmp/git_checkout
+cd /tmp/git_checkout
+git init
+git config core.sparseCheckout true
+git remote add origin "${GIT_REPO_URL}"
+echo "${TARGET_DIR}/*" >> .git/info/sparse-checkout
+git pull --depth 1 origin main
+
+cp -r ${TARGET_DIR}/* ${WEB_ROOT}/
+cd /
+rm -rf /tmp/git_checkout
+
+chown -R www-data:www-data ${WEB_ROOT}
+chmod -R 755 ${WEB_ROOT}
+
+apachectl configtest
+systemctl restart apache2
+systemctl enable apache2
